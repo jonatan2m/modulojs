36d2615c4ac767ba2871b42b4401b4e0535f6c2d
Jonatan Machado <jonatan.mac@gmail.com>
2014-08-28 08:53:48 -0300

    Update example to use unsubscribe with token

--- imports/mod.js
+++ imports/mod.js
@@ -1,10 +1,11 @@
-app.registerModule("mod", "./imports/mod.html", function (sb) {    
+app.registerModule("mod", "./imports/mod.html", function (sb) {
+    var mod = {};
     var count = 1;
-    sb.subscribe("evL", function (msg) { sb.extensions.log.info(msg); });
+    mod.tokenEvl = sb.subscribe("evL", function (msg) { sb.extensions.log.info(msg); });
             
     document.getElementById("load").onclick = function () {        
         sb.publish('evL', 'publish: ' + count);
-        sb.unsubscribe("evL");
+        sb.unsubscribe(mod.tokenEvl);
         count = 0;
     };
     document.getElementById("new").onclick = function () {
